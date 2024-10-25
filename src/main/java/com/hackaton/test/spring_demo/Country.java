96b7b95d08d150e970af8e145e44b881ce91b273
Maksym Norokha <maxnorohich@gmail.com>
2024-10-25 19:53:06 +0200

    Initial

---
+++ src/main/java/com/hackaton/test/spring_demo/Country.java
@@ -0,0 +1,16 @@
+package com.hackaton.test.spring_demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
+@Setter
+@Getter
+public class Country {
+    private String name;
+    private String capital;
+    private int population;
+}
