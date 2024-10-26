3b33ecaa2ab7ea56285c1449616d0476945ec8ae
Misha123345 <m.kwnyuka.m2005@gmail.com>
2024-10-26 16:30:34 +0200

    made chat for communication with the bot

---
+++ frontend/src/components/MessageBox/index.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import style from "./MessageBox.module.scss"
+
+type MessageBoxProps = {
+  isUser: boolean;
+  children: any;
+}
+
+const MessageBox: FC<MessageBoxProps> = ({isUser, children}) => {
+  return (
+    <div className={isUser ? `${style.box} ${style.user}` : `${style.box} ${style.bot}`}>
+      {children}
+    </div>
+  )
+}
+
+export default MessageBox;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,13 @@
 import React, { FC } from "react";
 import Header from "../../components/Header";
+import Popup from "../../components/Sidepanel";
 
 const Home: FC = () => {
   return (
-    <Header />
+    <>
+      <Header />
+      <Popup />
+    </>
   );
 };
 
